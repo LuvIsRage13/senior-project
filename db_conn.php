92aafa560cc6415b541283361b09e78b8772694c
LuvIsRage13 <you@example.com>
2023-09-19 14:21:52 -0400

    attempt to do php

---
+++ db_conn.php
@@ -0,0 +1,15 @@
+<?php
+
+$sname="localhost";
+$unmae="root";
+$password="";
+
+$db_name = "senior_project";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if(!$conn) {
+    echo "Connection failed";
+}else{
+    
+}
---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+include "db_conn.php";
+if(isset($_POST['uname']) && isset($_POST['password'])) {
+    function validate($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    $uname = validate($_POST['uname']);
+    $password = validate($_POST['password']);
+
+    $sql = "SELECT * FROM users WHERE uname = '$uname' AND password = '$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) === 1) {
+        $row = mysqli_fetch_assoc($result);
+
+        if($row['uname'] === $uname && $row['password'] === $password) {
+            echo "Logged in!"
+        }
+        else{
+            
+        }
+    }
+    else{
+        header("Location: index.php?error=Incorrect Username or Password");
+        exit();
+    }
+}else{
+    header("Location: index.php");
+    exit();
+}
+
